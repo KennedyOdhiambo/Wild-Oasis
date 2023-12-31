336b38c00fa7fe7b6cc896bb16de473b6b35a5be
kennedy <x.johnkennedy.x@gmail.com>
2023-12-31 19:50:12 +0300

    Mutations,deleting a cabin

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -30,7 +30,7 @@ const TableHeader = styled.header`
 
 const CabinTable = () => {
   const { isLoading, data: cabins } = useQuery({
-    queryKey: ["cabin"],
+    queryKey: ["cabins"],
     queryFn: getCabins,
   });
 
