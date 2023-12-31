231d6709b56b29f702232c244164e3aaf60db8bc
kennedy <x.johnkennedy.x@gmail.com>
2023-12-31 20:03:39 +0300

    Displaying Toast

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -41,6 +42,27 @@ const App = () => {
           </Route>
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 };
